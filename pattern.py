71492528c45abadd65c5e0dea738d75526210792
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-11-21 17:23:01 +0530

    patter_19

---
+++ pattern.py
@@ -0,0 +1,26 @@
+def pattern(n):
+    for i in range(n):
+        for j in range(n-i):
+            print("*",end="")
+        
+        
+        for j in range(2*i):
+            print(" ",end="")
+       
+        
+        for j in range(n-i):
+            print("*",end="")
+        print()
+        
+    for i in range(n):
+        for j in range(i+1):
+            print("*",end="")
+            
+        for j in range(2*(n-i-1)):
+            print(" ",end="")
+            
+        for j in range(i+1):
+            print("*",end="")
+            
+        print()
+pattern(5)
