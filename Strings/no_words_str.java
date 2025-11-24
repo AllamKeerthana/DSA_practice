54f730835277949c1d66457ad4bad9059049bc07
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-11-24 06:56:05 +0530

    no of words in a str

---
+++ Strings/no_words_str.java
@@ -0,0 +1,10 @@
+public class no_words_str{
+    public static void main(String[] args){
+        String s = "i am not so good";
+        String[] words = s.split(" ");
+        System.out.println(words.length);
+        // for(int i =0;i<words.length;i++){
+        //     System.out.println(words[i]);
+        // }
+    }
+}
