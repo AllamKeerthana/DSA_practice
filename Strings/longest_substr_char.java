b2918fe98a156a2004ad50b428035dade3fa3d4c
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-11-26 05:51:58 +0530

    longest substrin of same char

---
+++ Strings/longest_substr_char.java
@@ -0,0 +1,22 @@
+class long_substr_chr {
+    public static void main(String[] args) {
+        String s = "abcdaaabbbdfffff";
+        String longest = "";
+        String current = "" + s.charAt(0); // start with first character
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                current += s.charAt(i);
+            } else {
+                if (current.length() > longest.length()) {
+                    longest = current;
+                }
+                current = "" + s.charAt(i);
+            }
+        }
+        if (current.length() > longest.length()) {
+            longest = current;
+        }
+        System.out.println(longest);
+    }
+}
