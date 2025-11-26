8dcf0f30e2ba79d25b5d4e19cdc974b9bc7c8743
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-11-26 19:03:13 +0530

    min num in the arr

---
+++ Arrays/min_num.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class min_num {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("elements in the array");
+        for(int i : arr){
+            System.out.println(i);
+        }
+
+        int min = arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        
+        System.out.println("the max of the arr: " + min);
+    }
+}
+
+}
