f534f3bc11760857ec0567d7cad94586cbb8f63e
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-11-28 05:46:10 +0530

    rotate arr

---
+++ Arrays/rotation_arr.java
@@ -0,0 +1,23 @@
+public class rotation_arr {
+
+    public static void main(String[] args) {
+        int k = 6;
+        int[] arr = {1,2,3,4,5};
+        int n = arr.length;
+        int[] rot_arr = new int[n];
+        if(k>n) k = k%n;
+        if(k<n){
+        for(int i = k; i<n;i++){
+            rot_arr[i-k]=arr[i];
+        }
+        for(int i = 0; i<k;i++){
+            rot_arr[n-k+i]=arr[i];
+        }
+        }
+        
+        for(int i : rot_arr){
+            System.out.print(i+" ");
+        }
+    }
+}
+}
