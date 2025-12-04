fb386e7f56d3f8564ebe48f666eaa895b50114dc
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-12-04 15:34:43 +0530

    34

---
+++ LEETCODE/first_last_position.java
@@ -0,0 +1,17 @@
+public class first_last_position {
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (ans[0] == -1) ans[0] = i;  // first time found
+                ans[1] = i;                    // update every time
+            }
+        }
+
+        return ans;
+    }
+}
+
+
