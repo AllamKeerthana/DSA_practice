e637712959420e834fc26f204ce24ea3667f8a10
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-12-09 05:17:10 +0530

    66

---
+++ LEETCODE/plus_one.java
@@ -0,0 +1,18 @@
+public class plus_one {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // all digits were 9
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+
+    }
+}
