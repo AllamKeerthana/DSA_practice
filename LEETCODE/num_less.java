9489ed611cb21fd1c1c4d3ddaf521638977d7c85
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-12-15 15:26:31 +0530

    quest

---
+++ LEETCODE/num_less.java
@@ -0,0 +1,14 @@
+class num_less {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        
+        int[] arr = new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            for(int j =0;j<nums.length;j++){
+                if( i!=j && nums[i]>nums[j]) arr[i]++;
+            }
+            
+        }
+        return arr;
+    }
+    
+}
