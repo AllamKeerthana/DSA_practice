3ff9074b56374df621f4ea5ece7f4bbe051c58c4
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-12-23 13:15:26 +0530

    350

---
+++ LEETCODE/intersection_arr.java
@@ -0,0 +1,31 @@
+public class intersection_arr {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+       Arrays.sort(nums1);
+       Arrays.sort(nums2);
+        List<Integer> ans = new ArrayList<>();
+       int i=0,j=0;
+       while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                ans.add(nums1[i]);
+                i++;
+                j++;
+            }
+
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+       }
+
+        int[] arr = new int[ans.size()];
+        int a=0;
+        for(int x : ans){
+            arr[a] = x;
+            a++;
+        }
+        return arr;
+    }
+}
