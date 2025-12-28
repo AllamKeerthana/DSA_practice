f919c6db3439cad3a174d7838fd39a1b839516cd
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2025-12-28 15:19:07 +0530

    167

---
+++ LEETCODE/two_sumII.java
@@ -0,0 +1,25 @@
+public class two_sumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+       int left = 0, right = numbers.length-1;
+       int[] arr = new int[2];
+       while(left<=right){
+        
+        if(numbers[left]+numbers[right]==target){
+            arr[0]=left+1;
+            arr[1]=right+1;
+            return arr;
+        }
+
+        else if(numbers[left]+numbers[right]<target){
+            left++;
+        }
+
+        else {
+            right--;
+        }
+        
+       } 
+       return arr;
+    }
+}
