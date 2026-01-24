32c4f9a05ed27ad0a6a89e7c131233a5d60a269b
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-01-24 21:59:12 +0530

    14

--- LEETCODE/lng_com_prefix.java
+++ LEETCODE/lng_com_prefix.java
@@ -1,14 +1,14 @@
 class lng_com_prefix {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-
+        // consider the first string to traverse character by character
         String first = strs[0];
 
         for (int i = 0; i < first.length(); i++) {
             char ch = first.charAt(i);
 
             for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != ch) { // if index exceeds length of any string or characters don't match
                     return first.substring(0, i);
                 }
             }
