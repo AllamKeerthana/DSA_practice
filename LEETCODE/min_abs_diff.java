f676084ce923ead17cd68ea95cc160faf1e4bfc5
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-01-26 08:13:57 +0530

    1200

---
+++ LEETCODE/min_abs_diff.java
@@ -0,0 +1,26 @@
+public class min_abs_diff {
+import java.util.*;
+
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        Arrays.sort(arr); 
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            min = Math.min(min, arr[i + 1] - arr[i]);
+        }
+
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] == min) {
+                ans.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+
+        return ans;
+    }
+}
+
