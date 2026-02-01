6f0cd19e423f1ee17db89faa8a01fcad391e3b47
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-02-01 09:23:57 +0530

    206

---
+++ LEETCODE/rev_ll.java
@@ -0,0 +1,28 @@
+public class rev_ll {
+
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        
+
+        while( curr!= null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            if(next!=null){
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
