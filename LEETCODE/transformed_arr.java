38c57f39145fa412fb2e958c26e43ca9388bbe81
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-02-05 07:01:31 +0530

    3379

---
+++ LEETCODE/transformed_arr.java
@@ -0,0 +1,23 @@
+public class transformed_arr {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } 
+            else {
+                int newIndex = (i + nums[i]) % n;
+                
+                if (newIndex < 0) {
+                    newIndex += n;
+                }
+
+                result[i] = nums[newIndex];
+            }
+        }
+        return result;
+    }
+}
