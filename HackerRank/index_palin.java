da2f3f07260fb42dda3932b26dd72986deba2a1a
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-02-23 23:18:47 +0530

    23rd feb

---
+++ HackerRank/index_palin.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+    // Write your code here
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+
+                // Try removing left character
+                if (isPalindrome(s, left + 1, right)) {
+                    return left;
+                }
+
+                // Try removing right character
+                if (isPalindrome(s, left, right - 1)) {
+                    return right;
+                }
+
+                // Not possible
+                return -1;
+            }
+        }
+
+        // Already palindrome
+        return -1;
+    }
+
+    // Helper function
+    private static boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+
+
