2eeec4f95b254aa64fef267766a7915b06ca9348
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-05-08 08:31:42 +0530

    sorting

--- Arrays/sorting.java
+++ Arrays/sorting.java
@@ -8,6 +8,8 @@ public class sorting {
         System.out.println("Sorted array (selection sort): " + Arrays.toString(arr));
         insertion(arr);
         System.out.println("Sorted array (insertion sort): " + Arrays.toString(arr));
+        int[] merged = mergesort(arr);
+        System.out.println("Sorted array (merge sort): " + Arrays.toString(merged));
     }
 
     static void bubble(int[] arr){
@@ -52,5 +54,45 @@ public class sorting {
                 else break;
             }
         }
+    }
+
+    static int[] mergesort(int[] arr){
+        if(arr.length==1) return arr;
+
+        int mid = arr.length/2;
+        int[] left = mergesort(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergesort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+
+    }
+
+    static int[] merge(int[] left, int[] right){
+        int[] ans  = new int[left.length+right.length];
+        int i = 0, j = 0, k = 0;
+        while(i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                ans[k]=left[i];
+                i++;
+                k++;
+            }
+            else{
+                ans[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i<left.length){
+            ans[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j<right.length){
+            ans[k] = right[j];
+            j++;
+            k++;
+        }
+        return ans;
     }
 }
