aa0ea44cf73aaabb71b4d465b8f5bf9e192e8b0a
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-07-02 22:26:32 +0530

    268

---
+++ LEETCODE/missing_num.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        // int sum = 0;
+        // int total = n*(n+1)/2;
+
+        // for(int i: nums){
+        //     sum+=i;
+        // }
+        // return total - sum;
+        int xor = n;
+        for(int i = 0;i<n;i++){
+            xor = xor^i^nums[i];
+        }
+        return xor;
+    }
+}
