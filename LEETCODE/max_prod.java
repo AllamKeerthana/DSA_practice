5f56172771a960b7029fdea742e9c49dbb2e15c3
AllamKeerthana <keerthanachowdaryallam@gmail.com>
2026-07-25 18:31:36 +0530

    3536\

--- LEETCODE/max_prod.java
+++ LEETCODE/max_prod.java
@@ -1,16 +1,35 @@
+// class Solution {
+//     public int maxProduct(int n) {
+//         PriorityQueue<Integer> list = new PriorityQueue<>(Collections.reverseOrder());
+//         int org = n;
+//         while(org>0){
+//             int rem = org%10;
+//             list.offer(rem);
+//             org/=10;
+//         }
+
+//         int pro = 1;
+//         pro*=list.poll();
+//         pro*=list.poll();
+//         return pro;   
+//     }
+// }
 class Solution {
     public int maxProduct(int n) {
-        PriorityQueue<Integer> list = new PriorityQueue<>(Collections.reverseOrder());
-        int org = n;
-        while(org>0){
-            int rem = org%10;
-            list.offer(rem);
-            org/=10;
+        int max1 = 0;
+        int max2 = 0;
+        while(n>0){
+            int rem = n%10;
+            if(rem>max1){
+                max2 = max1;
+                max1=rem;
+            }
+            else if(rem<=max1 && rem>max2){
+                max2 = rem;
+            }
+            n/=10;
         }
 
-        int pro = 1;
-        pro*=list.poll();
-        pro*=list.poll();
-        return pro;   
+        return max1*max2;
     }
 }
